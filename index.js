9204404f79ee1cd841a4ab49a9b447c4ac5997c4
Taimur Shah <tshah@scu.edu>
2018-10-16 19:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") { 
   console.log("The name is correct")
+  else {
+    console.log("That's the wrong number")
+  }
 }
 
 // Don't worry about this
