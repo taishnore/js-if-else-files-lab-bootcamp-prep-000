37d3999e218f27956f38d3505dc7e325177c88df
Taimur Shah <tshah@scu.edu>
2018-10-16 19:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") { 
   console.log("The name is correct")
-  else {
+  } else {
     console.log("That's the wrong number")
   }
-}
+
 
 // Don't worry about this
 module.exports = { name, height
